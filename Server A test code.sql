619b1d5770aead2a69fc0affd70f8f9a7b0acedd
yjwipod <yjwipod@163.com>
2017-03-29 01:34:09 +1100

    Create Server A test code.sql

---
+++ Server A test code.sql
@@ -0,0 +1,6 @@
+insert into hotel values ('0001', 'Hotel No.1', '5 stars', 2015, 'Australia', 'Melbourne', 'Swanston St, VIC', '05135987', 'htno1@hotel.com');
+insert into hotel values ('0002', 'Hotel No.2', '4 stars', 2013, 'Australia', 'Melbourne', 'Bourke St, VIC', '05136936', 'htno2@hotel.com');
+
+
+commit;
+select * from hotel
